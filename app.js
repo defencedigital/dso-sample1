733a4d7adad392ed56bd3f716899c76a9ff3a109
semirmussa1 <s.mussa@accenture.com>
2022-08-11 11:14:43 +0100

    initial content

---
+++ app.js
@@ -0,0 +1,13 @@
+//const express = require('express')
+//const app = express()
+//app.get('/', function(request,result){
+    
+    // result.send('Hello World!! from ')
+    var name = prompt("Enter your name", "Enter name");
+    document.write("Hello " + name + "!");
+    //document.getElementById("demo").innerHTML = "Hello Connor!" + name123;
+    console.log('hello world app is listening on port 3333')
+//})
+app.listen(3333,"0.0.0.0",function(){
+   console.log('hello world app is listening on port 3333')
+
